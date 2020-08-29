8e3f97524433a8de85ee50c8128882daafd9b982
SaraCremonesi <saracrem00@gmail.com>
2020-08-29 19:07:14 +0200

    completamento esercizio

--- index.php
+++ index.php
@@ -13,8 +13,14 @@
     <h1>Stanze Boolean Hotel</h1>
     <?php foreach ($rooms as $room) { ?>
     <ul>
-      <?php $room = new Stanza($room['id'], $room['room_number'], $room['floor'], $room['beds'], $room['created_at'], $room['updated_at']) ?>
-      <li><?php echo $room->getRoomData(); ?></li>
+      <?php $room = new Stanze ($room['id'], $room['room_number'], $room['floor'], $room['beds'], $room['created_at'], $room['updated_at']); ?>
+      <?php $room_print = $room->getRoomData(); ?>
+      <li>Id stanza: <?php echo $room_print['id']; ?></li>
+      <li>Numero stanza: <?php echo $room_print['room_number']; ?></li>
+      <li>Piano: <?php echo $room_print['floor']; ?></li>
+      <li>Numero letti: <?php echo $room_print['beds']; ?></li>
+      <li>Creata il: <?php echo $room_print['created_at']; ?></li>
+      <li>Aggiornata il: <?php echo $room_print['updated_at']; ?></li>
     </ul>
       <?php } ?>
   </body>
